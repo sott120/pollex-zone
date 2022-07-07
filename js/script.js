af47d3eefa7d9b947164593d4b7d5d66bd7aea7a
sott120 <psy_sott120@naver.com>
2022-07-07 17:51:00 +0900

    메인페이지 js 추가

--- js/script.js
+++ js/script.js
@@ -1,32 +1,39 @@
 const cenIcon = document.querySelectorAll('.icon');
 const cenImg = document.querySelectorAll('.cen_img');
+const cenTxt = document.querySelectorAll('.centxt');
+const iconTxt = document.querySelectorAll('.icon_txt');
 
-cenIcon[0].addEventListener('mouseover',imgOn1);
-cenIcon[1].addEventListener('mouseover',imgOn2);
-cenIcon[2].addEventListener('mouseover',imgOn3);
+cenIcon.forEach(function(li){
+    li.addEventListener('mouseover',function(e){
+        let num = e.currentTarget.dataset.num;
+        imgOn(num);
+    });
+})
 
-
-function imgOn1(){
-    cenImg[0].classList.add('on');
-    cenImg[1].classList.remove('on');
-    cenImg[2].classList.remove('on');
-}
-function imgOn2(){
-    cenImg[0].classList.remove('on');
-    cenImg[1].classList.add('on');
-    cenImg[2].classList.remove('on');
-}
-
-function imgOn3(){
+function imgOn(n){
     cenImg[0].classList.remove('on');
     cenImg[1].classList.remove('on');
-    cenImg[2].classList.add('on');
+    cenImg[2].classList.remove('on');
+    cenImg[n].classList.add('on');
+    cenTxt[0].classList.remove('on');
+    cenTxt[1].classList.remove('on');
+    cenTxt[2].classList.remove('on');
+    cenTxt[n].classList.add('on');
+    iconTxt[0].classList.remove('on');
+    iconTxt[1].classList.remove('on');
+    iconTxt[2].classList.remove('on');
+    iconTxt[n].classList.add('on');
 }
 
-// cenIcon[n].addEventListener('mouseover',imgOn);
-// function imgOn(n){
-//     cenImg[0].classList.remove('on');
-//     cenImg[1].classList.remove('on');
-//     cenImg[2].classList.remove('on');
-//     cenImg[n].classList.add('on');
-// }
+const squareWrap = document.getElementById('squareWrap');
+const aniBox = document.querySelectorAll('.aniBox');
+
+function squareCont() {
+    for(i = 0; i<aniBox.length; i++){
+    if (window.innerHeight > aniBox[i].getBoundingClientRect().top + 100) {
+        aniBox[i].classList.add('gogo');
+        }
+    }
+}
+
+window.addEventListener('scroll', squareCont);
