c8612b51edb43df8f6107c603f47a163f203c578
sott120 <psy_sott120@naver.com>
2022-07-08 13:57:14 +0900

    햄버거 네비게이션 추가

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//호버시 이미지,텍스트 변경
 const cenIcon = document.querySelectorAll('.icon');
 const cenImg = document.querySelectorAll('.cen_img');
 const cenTxt = document.querySelectorAll('.centxt');
@@ -25,6 +26,7 @@ function imgOn(n){
     iconTxt[n].classList.add('on');
 }
 
+//스크롤시 네모배너 애니메이션
 const squareWrap = document.getElementById('squareWrap');
 const aniBox = document.querySelectorAll('.aniBox');
 
@@ -38,18 +40,33 @@ function squareCont() {
 
 window.addEventListener('scroll', squareCont);
 
-
+//햄버거메뉴 클릭
 const ham = document.getElementById('ham'); 
 const hamChild = ham.childNodes;
-console.log(ham);
-console.log(hamChild);
+const logoColor = document.getElementsByClassName('logo_color')[0];
+const logoWhite = document.getElementsByClassName('logo_white')[0];
+const menuWrap = document.getElementsByClassName('allMenuWrap')[0];
 
 ham.addEventListener('click',hamClick);
 
 function hamClick(){
     if(ham.classList.contains('active') === true){
     ham.classList.remove('active');
+    logoColor.style.opacity = 1;
+    logoWhite.style.opacity = 0;
+    menuWrap.style.display = 'none';
     } else{
         ham.classList.add('active');
+        logoColor.style.opacity = 0;
+        logoWhite.style.opacity = 1;
+        menuWrap.style.display = 'block';
     }
 }
+
+//네비게이션 리스트 클릭
+const test = document.getElementsByClassName('allMenu')[0].childNodes;
+console.log(test);
+
+// document.querySelector('#toggle-button').addEventListener('click', function() {
+//     document.querySelector('.section.collapsible').classList.toggle('collapsed');
+//   });
