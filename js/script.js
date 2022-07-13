0bc0f425d0a9fd7c1b3f8bec014b291e5b5d8a77
sott120 <psy_sott120@naver.com>
2022-07-13 17:11:09 +0900

    모바일 네비게이션 아코디언 추가중

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ const cenImg = document.querySelectorAll('.cen_img');
 const cenTxt = document.querySelectorAll('.centxt');
 const iconTxt = document.querySelectorAll('.icon_txt');
 
-cenIcon.forEach(function(li){
-    li.addEventListener('mouseover',function(e){
+cenIcon.forEach(function (li) {
+    li.addEventListener('mouseover', function (e) {
         let num = e.currentTarget.dataset.num;
         imgOn(num);
     });
-})
+});
 
-function imgOn(n){
+function imgOn(n) {
     cenImg[0].classList.remove('on');
     cenImg[1].classList.remove('on');
     cenImg[2].classList.remove('on');
@@ -31,9 +31,9 @@ const squareWrap = document.getElementById('squareWrap');
 const aniBox = document.querySelectorAll('.aniBox');
 
 function squareCont() {
-    for(i = 0; i<aniBox.length; i++){
-    if (window.innerHeight > aniBox[i].getBoundingClientRect().top + 100) {
-        aniBox[i].classList.add('gogo');
+    for (i = 0; i < aniBox.length; i++) {
+        if (window.innerHeight > aniBox[i].getBoundingClientRect().top + 100) {
+            aniBox[i].classList.add('gogo');
         }
     }
 }
@@ -41,21 +41,21 @@ function squareCont() {
 window.addEventListener('scroll', squareCont);
 
 //햄버거메뉴 클릭
-const ham = document.getElementById('ham'); 
+const ham = document.getElementById('ham');
 const hamChild = ham.childNodes;
 const logoColor = document.getElementsByClassName('logo_color')[0];
 const logoWhite = document.getElementsByClassName('logo_white')[0];
 const menuWrap = document.getElementsByClassName('allMenuWrap')[0];
 
-ham.addEventListener('click',hamClick);
+ham.addEventListener('click', hamClick);
 
-function hamClick(){
-    if(ham.classList.contains('active') === true){
-    ham.classList.remove('active');
-    logoColor.style.opacity = 1;
-    logoWhite.style.opacity = 0;
-    menuWrap.style.display = 'none';
-    } else{
+function hamClick() {
+    if (ham.classList.contains('active') === true) {
+        ham.classList.remove('active');
+        logoColor.style.opacity = 1;
+        logoWhite.style.opacity = 0;
+        menuWrap.style.display = 'none';
+    } else {
         ham.classList.add('active');
         logoColor.style.opacity = 0;
         logoWhite.style.opacity = 1;
@@ -64,9 +64,22 @@ function hamClick(){
 }
 
 //네비게이션 리스트 클릭
-const test = document.getElementsByClassName('allMenu')[0].childNodes;
-console.log(test);
+const test = document.querySelectorAll('.mTitle');
 
-// document.querySelector('#toggle-button').addEventListener('click', function() {
-//     document.querySelector('.section.collapsible').classList.toggle('collapsed');
-//   });
+let i;
+// test[0].addEventListener('click', function () {
+//     mToggle[0].classList.toggle('active');
+// });
+
+for (i = 0; i < test.length; i++) {
+    test[i].addEventListener('click', function () {
+        const mToggle = document.querySelectorAll('.mToggle');
+        mToggle[0].classList.add('active');
+        console.log(mToggle[0]);
+        if (mToggle[0].style.maxHeight) {
+            mToggle[0].style.maxHeight = null;
+        } else {
+            mToggle[0].style.maxHeight = mToggle[0].scrollHeight + 'px';
+        }
+    });
+}
